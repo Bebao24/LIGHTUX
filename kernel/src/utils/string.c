3589c737e1836cc305ae05d761a928ac8ab17ddb
Bebao24 <baong2408@gmail.com>
2025-06-04 21:50:29 +0700

    Implemented basic string manipulation & moved memory.h and memory.c into util

---
+++ kernel/src/utils/string.c
@@ -0,0 +1,81 @@
+#include "string.h"
+
+const char* strchr(const char* string, char c)
+{
+    if (string == NULL)
+    {
+        return NULL;
+    }
+
+    while (*string)
+    {
+        if (*string == c)
+        {
+            return string;
+        }
+
+        string++;
+    }
+
+    return NULL;
+}
+
+char* strcpy(char* dst, const char* src)
+{
+    char* originalDst = dst;
+
+    if (dst == NULL)
+    {
+        return NULL;
+    }
+
+    if (src == NULL)
+    {
+        *dst = '\0';
+        return NULL;
+    }
+
+    while (*src)
+    {
+        *dst = *src;
+        src++;
+        dst++;
+    }
+
+    *dst = '\0';
+    return originalDst;
+}
+
+size_t strlen(const char* string)
+{
+    size_t len = 0;
+
+    while (*string)
+    {
+        len++;
+        string++;
+    }
+
+    return len;
+}
+
+int strcmp(const char* a, const char* b)
+{
+    if (a == NULL && b == NULL)
+    {
+        return 0;
+    }
+
+    if (a == NULL | b == NULL)
+    {
+        return -1;
+    }
+
+    while (*a && *b && *a == *b)
+    {
+        ++a;
+        ++b;
+    }
+    return (*a) - (*b);
+}
+
---
+++ kernel/src/utils/string.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <stddef.h>
+
+const char* strchr(const char* string, char c);
+char* strcpy(char* dst, const char* src);
+size_t strlen(const char* string);
+int strcmp(const char* a, const char* b);
